a06601e90420703fbd0edec2260b230afec4d214
Laura <laura.hoerburger@gmail.com>
2025-01-28 16:47:14 +0100

    adding "single string"

---
+++ simple_strings.py
@@ -0,0 +1,12 @@
+s1 = "John" #double quote
+s2 = 'Mary' #singke quote
+
+print(s1)
+print(s2)
+
+s1 = "And then he asked: 'How are you?'"
+print(s1)
+s1 = 'And then he asked: "How are you?"'
+print(s1)
+s1 = "this is how to have \" and \' inside a string" #you can also escape them
+print(s1)
